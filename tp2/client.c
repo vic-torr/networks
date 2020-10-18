0db46c427954830bf1623aab0972a3be391bfa4e
Victor Moraes <victor.moraes@enacom.com.br>
2020-10-18 20:04:03 -0300

    analysis for each test batch

--- tp2/client.c
+++ tp2/client.c
@@ -1,48 +1,56 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<netdb.h>
-#include<string.h>
-#include<unistd.h>
-#include<arpa/inet.h>
-#include<time.h>
-#include<sys/time.h>
-#include<math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <sys/time.h>
+#include <math.h>
 
 #define SERVER_PORT 54321
 #define MAX_LINE 256
 #define N_BYTES 32000
 
 /*Create Message*/
-char * gen_str(int msg_size){
-    char * msg;
+char *gen_str(int msg_size)
+{
+    char *msg;
     msg = malloc(sizeof(char) * msg_size);
     int i;
-    for(i=0;i<msg_size - 1;i++){
-        msg[i]='a';
+    for (i = 0; i < msg_size - 1; i++)
+    {
+        msg[i] = 'a';
     }
     msg[msg_size - 1] = '\0';
     return msg;
 }
 
 /*Save results in file*/
-int save_to_file(unsigned n,unsigned m,double mat[n][m],double mat2[n],double mat3[n]){
-FILE * pFile;
-pFile = fopen("result.csv","w");
-if(pFile!=NULL)
+int save_to_file(unsigned n, unsigned m, double mat[n][m], double mat2[n], double mat3[n], char *file_name)
 {
-    fprintf(pFile,"tamanho(bytes),latencia(s),vazao(bps), Lost Packages");
-    int i;
-    for(i=0;i<n;i++){
-        fprintf(pFile,"\n %f, %f, %f, %f",mat[i][0],mat[i][1],mat2[i],mat3[i]);
+    FILE *pFile;
+
+    pFile = fopen(file_name, "w");
+
+    puts(file_name);
+    if (pFile != NULL)
+    {
+        fprintf(pFile, "tamanho(bytes),latencia(s),vazao(bps), Lost Packages");
+        int i;
+        for (i = 0; i < n; i++)
+        {
+            fprintf(pFile, "\n %f, %f, %f, %f", mat[i][0], mat[i][1], mat2[i], mat3[i]);
+        }
+        fclose(pFile);
     }
-    fclose(pFile);
-}
 }
 
-int main(int argc,char * argv[]){
+int main(int argc, char *argv[])
+{
     char *host;
     int s;
     int len;
@@ -53,113 +61,138 @@ int main(int argc,char * argv[]){
     struct sockaddr_in sin;
     int status;
     fd_set read_sd_set;
-    double latencia[43][2]= {{1,0},{100,0},{200,0},{300,0},{400,0},{500,0},{600,0}, {700,0},{800,0},{900,0},{1000,0},{300,0},{400,0},{500,0},{600,0},{700,0},{800,0},{900,0},{100,0},{300,0},{400,0}, {500,0},{600,0},{700,0},{800,0},{900,0},{100,0},{300,0},{400,0},{500,0},{600,0},{700,0},{800,0},{900,0},{100,0}, {300,0},{400,0},{500,0},{600,0},{700,0},{800,0},{900,0}, {100,0}};
+    double latencia[43][2] = {{1, 0}, {100, 0}, {200, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {1000, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {100, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {100, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {100, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {100, 0}};
     double vazao[43];
     double contaPacote[43];
     int it;
 
-    /*Start latencia*/
-    for(it=0;it<32;it++)
+    // Get last test
+
+    char *file_name = calloc(10, 1);
+    int experiment_number = 0;
+    sprintf(file_name, "result_%d.csv", experiment_number);
+    puts(file_name);
+
+    while (fopen(file_name, "r") != NULL) // check if file exists
     {
-        latencia[it+11][0]=(it+1) * pow(2,10);
+        experiment_number++;
+        sprintf(file_name, "result_%d.csv", experiment_number);
+        puts(file_name);
+    }
+
+    puts(file_name);
+
+    /*Start latency*/
+    for (it = 0; it < 32; it++)
+    {
+        latencia[it + 11][0] = (it + 1) * pow(2, 10);
     }
 
     /*Get IP address*/
-    host=argv[1];
-    if(argc==2){
-        host=argv[1];
+    host = argv[1];
+    if (argc == 2)
+    {
+        host = argv[1];
     }
-    else{
+    else
+    {
         fprintf(stderr, "Uso: simplex-talk host \n");
         exit(1);
     }
 
     /*Create socket*/
-    s=socket(AF_INET,SOCK_DGRAM,0);
-    if(s<0){
+    s = socket(AF_INET, SOCK_DGRAM, 0);
+    if (s < 0)
+    {
         perror("simplex-talk: socket");
         exit(1);
     }
 
     /*Get host name and data structure*/
-    server=gethostbyname(host);
-    if(server==NULL){
-        fprintf(stderr,"simplex-talk: unknown host: %s \n",host);
+    server = gethostbyname(host);
+    if (server == NULL)
+    {
+        fprintf(stderr, "simplex-talk: unknown host: %s \n", host);
         exit(1);
     }
     bzero((char *)&sin, sizeof(sin));
     sin.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, (char*)&sin.sin_addr.s_addr, server->h_length);
+    bcopy((char *)server->h_addr, (char *)&sin.sin_addr.s_addr, server->h_length);
     sin.sin_port = htons(SERVER_PORT);
-    server_len=sizeof(sin);
+    server_len = sizeof(sin);
 
     /*Variables*/
-    double aux_lat[3];//Latency vector
-    int size,size_it;
-    int i,j;
-    int n_it=100000;//Number of Iterations
-    int conta=0;
+    double aux_lat[3]; //Latency vector
+    int size, size_it;
+    int i, j;
+    int n_it = 100000; //Number of Iterations
+    int conta = 0;
     double elapsedTime;
 
     /*Test for each package size*/
-    for(size_it=0; size_it<43; size_it++){
+    for (size_it = 0; size_it < 43; size_it++)
+    {
 
         /*Create message*/
         size = latencia[size_it][0];
-        char * buf = gen_str(size);
+        char *buf = gen_str(size);
         printf("\n\n Packe Size: %d Bytes", size);
 
         /*Start lost packages count*/
-        conta=0;
+        conta = 0;
 
         /*Three reps for test*/
-        for(j=0;j<3;j++){
+        for (j = 0; j < 3; j++)
+        {
 
             /*Get time*/
-            gettimeofday(&t1,NULL);
+            gettimeofday(&t1, NULL);
 
             /*Start Test*/
-            for(i=0;i<n_it;i++){
+            for (i = 0; i < n_it; i++)
+            {
 
                 /*Send Message*/
-                count=sendto(s,buf,strlen(buf),0,(struct sockaddr *) &sin, server_len);
-                if(count<0)
+                count = sendto(s, buf, strlen(buf), 0, (struct sockaddr *)&sin, server_len);
+                if (count < 0)
                     perror("Erro no envio");
-                    FD_ZERO (&read_sd_set);
-                    FD_SET (s,&read_sd_set);
-                    tv.tv_sec = 1;
-                    tv.tv_usec = 0;
-                    status = select(s+1,&read_sd_set,NULL,NULL,&tv);
-                    if(FD_ISSET(s, &read_sd_set)){
+                FD_ZERO(&read_sd_set);
+                FD_SET(s, &read_sd_set);
+                tv.tv_sec = 1;
+                tv.tv_usec = 0;
+                status = select(s + 1, &read_sd_set, NULL, NULL, &tv);
+                if (FD_ISSET(s, &read_sd_set))
+                {
 
-                        /*Get Message*/
-                        count = recvfrom(s,buf,strlen(buf),0,(struct sockaddr *) &sin, &server_len);
-                        if(count<0)
-                            perror("Erro na recepcao");
-                        }
-                        else{
-                            printf("TIMEOUT UDP");
-                            conta=conta+1;
-                        }
-                    }
-
-                /*Lantecy in ms*/
-                gettimeofday(&t2, NULL);
-                elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;
-                elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;
-                aux_lat[j] = elapsedTime / n_it;
+                    /*Get Message*/
+                    count = recvfrom(s, buf, strlen(buf), 0, (struct sockaddr *)&sin, &server_len);
+                    if (count < 0)
+                        perror("Erro na recepcao");
+                }
+                else
+                {
+                    printf("TIMEOUT UDP");
+                    conta = conta + 1;
+                }
             }
 
-            /*Measures avarage*/
-            latencia[size_it][1]=(aux_lat[0] + aux_lat[1] + aux_lat[2]) / 3;
-            vazao[size_it]=(latencia[size_it][0] * 8) / latencia[size_it][1];
-            contaPacote[size_it]=conta;
-            printf("\n MeanLatency: %.f miliseconds \n",latencia[size_it][1]);
-            printf("\n Mean Throughput: %f bps\n",vazao[size_it]);
-
-            /*Save Results*/
-            save_to_file(43,2,latencia,vazao,contaPacote);
-            free(buf);
+            /*Lantecy in ms*/
+            gettimeofday(&t2, NULL);
+            elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;
+            elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;
+            aux_lat[j] = elapsedTime / n_it;
         }
+
+        /*Measures avarage*/
+        latencia[size_it][1] = (aux_lat[0] + aux_lat[1] + aux_lat[2]) / 3;
+        vazao[size_it] = (latencia[size_it][0] * 8) / latencia[size_it][1];
+        contaPacote[size_it] = conta;
+        printf("\n MeanLatency: %.f miliseconds \n", latencia[size_it][1]);
+        printf("\n Mean Throughput: %f bps\n", vazao[size_it]);
+
+        /*Save Results*/
+        save_to_file(43, 2, latencia, vazao, contaPacote, file_name);
+        free(buf);
+    }
     close(s);
 }
