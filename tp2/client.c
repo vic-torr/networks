62a734b2ed15df165ba7fbf3f864049a6ccf6e51
Victor Moraes <victor.moraes@enacom.com.br>
2020-10-18 22:10:21 -0300

    refactorization

--- tp2/client.c
+++ tp2/client.c
@@ -1,36 +1,23 @@
+#include <math.h>
+#include <sys/types.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/time.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <time.h>
-#include <sys/time.h>
-#include <math.h>
+#include <sys/socket.h>
 
-#define SERVER_PORT 54321
+#define SERVER_PORT 33333
 #define MAX_LINE 256
 #define N_BYTES 32000
 
-/*Create Message*/
-char *gen_str(int msg_size)
-{
-    char *msg;
-    msg = malloc(sizeof(char) * msg_size);
-    int i;
-    for (i = 0; i < msg_size - 1; i++)
-    {
-        msg[i] = 'a';
-    }
-    msg[msg_size - 1] = '\0';
-    return msg;
-}
 
-/*Save results in file*/
-int save_to_file(unsigned n, unsigned m, double mat[n][m], double mat2[n], double mat3[n], char *file_name)
+// Save results in file
+int save_to_file(unsigned n, unsigned m, float mat[n][m], float mat2[n], float mat3[n], char *file_name)
 {
     FILE *pFile;
 
@@ -39,7 +26,7 @@ int save_to_file(unsigned n, unsigned m, double mat[n][m], double mat2[n], doubl
     puts(file_name);
     if (pFile != NULL)
     {
-        fprintf(pFile, "tamanho(bytes),latencia(s),vazao(bps), Lost Packages");
+        fprintf(pFile, "tamanho(bytes),latencia(s),vazao(bps), Lost packet");
         int i;
         for (i = 0; i < n; i++)
         {
@@ -48,6 +35,20 @@ int save_to_file(unsigned n, unsigned m, double mat[n][m], double mat2[n], doubl
         fclose(pFile);
     }
 }
+
+// Create Message
+char *generate_message(int size)
+{
+    char *message;
+    message = malloc(sizeof(char) * size);
+    for (int i = 0; i < size - 1; i++)
+    {
+        message[i] = 'a'+(i*17)%26;
+    }
+    message[size - 1] = '\0';
+    return message;
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -61,10 +62,19 @@ int main(int argc, char *argv[])
     struct sockaddr_in sin;
     int status;
     fd_set read_sd_set;
-    double latencia[43][2] = {{1, 0}, {100, 0}, {200, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {1000, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {100, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {100, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {100, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {100, 0}};
-    double vazao[43];
-    double contaPacote[43];
+    float latencia[43][2] = {{1, 0}, {100, 0}, {200, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {1000, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {100, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {100, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {100, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {100, 0}};
+    int latency_packet_size[20];
+    float latency_time[20];
+    float vazao[43];
+    float contaPacote[43];
     int it;
+        // Variables
+    float aux_lat; //Latency
+    int size, size_it;
+    int i, j;
+    int n_it = 100; //Number of Iterations
+    int conta = 0;
+    float elapsedTime;
 
     // Get last test
 
@@ -82,13 +92,13 @@ int main(int argc, char *argv[])
 
     puts(file_name);
 
-    /*Start latency*/
+    // Start latency
     for (it = 0; it < 32; it++)
     {
         latencia[it + 11][0] = (it + 1) * pow(2, 10);
     }
 
-    /*Get IP address*/
+    // Get IP address
     host = argv[1];
     if (argc == 2)
     {
@@ -100,7 +110,7 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    /*Create socket*/
+    // Create socket
     s = socket(AF_INET, SOCK_DGRAM, 0);
     if (s < 0)
     {
@@ -108,7 +118,7 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    /*Get host name and data structure*/
+    // Get host name and data structure
     server = gethostbyname(host);
     if (server == NULL)
     {
@@ -121,76 +131,65 @@ int main(int argc, char *argv[])
     sin.sin_port = htons(SERVER_PORT);
     server_len = sizeof(sin);
 
-    /*Variables*/
-    double aux_lat[3]; //Latency vector
-    int size, size_it;
-    int i, j;
-    int n_it = 100000; //Number of Iterations
-    int conta = 0;
-    double elapsedTime;
 
-    /*Test for each package size*/
+
+    // Test for each package size
     for (size_it = 0; size_it < 43; size_it++)
     {
 
-        /*Create message*/
+        // Create message
         size = latencia[size_it][0];
-        char *buf = gen_str(size);
-        printf("\n\n Packe Size: %d Bytes", size);
+        char *buf = generate_message(size);
+        printf("\n\n Packet Size: %d Bytes", size);
 
-        /*Start lost packages count*/
+        // Start lost packet count
         conta = 0;
 
-        /*Three reps for test*/
-        for (j = 0; j < 3; j++)
+        // Get time
+        gettimeofday(&t1, NULL);
+
+        // Start Test
+        for (i = 0; i < n_it; i++)
         {
 
-            /*Get time*/
-            gettimeofday(&t1, NULL);
-
-            /*Start Test*/
-            for (i = 0; i < n_it; i++)
+            // Send Message
+            count = sendto(s, buf, strlen(buf), 0, (struct sockaddr *)&sin, server_len);
+            if (count < 0)
+                perror("Erro no envio");
+            FD_ZERO(&read_sd_set);
+            FD_SET(s, &read_sd_set);
+            tv.tv_sec = 1;
+            tv.tv_usec = 0;
+            status = select(s + 1, &read_sd_set, NULL, NULL, &tv);
+            if (FD_ISSET(s, &read_sd_set))
             {
 
-                /*Send Message*/
-                count = sendto(s, buf, strlen(buf), 0, (struct sockaddr *)&sin, server_len);
+                // Get Message
+                count = recvfrom(s, buf, strlen(buf), 0, (struct sockaddr *)&sin, &server_len);
                 if (count < 0)
-                    perror("Erro no envio");
-                FD_ZERO(&read_sd_set);
-                FD_SET(s, &read_sd_set);
-                tv.tv_sec = 1;
-                tv.tv_usec = 0;
-                status = select(s + 1, &read_sd_set, NULL, NULL, &tv);
-                if (FD_ISSET(s, &read_sd_set))
-                {
-
-                    /*Get Message*/
-                    count = recvfrom(s, buf, strlen(buf), 0, (struct sockaddr *)&sin, &server_len);
-                    if (count < 0)
-                        perror("Erro na recepcao");
-                }
-                else
-                {
-                    printf("TIMEOUT UDP");
-                    conta = conta + 1;
-                }
+                    perror("Erro na recepcao");
             }
-
-            /*Lantecy in ms*/
-            gettimeofday(&t2, NULL);
-            elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;
-            elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;
-            aux_lat[j] = elapsedTime / n_it;
+            else
+            {
+                printf("TIMEOUT UDP");
+                conta = conta + 1;
+            }
         }
 
-        /*Measures avarage*/
-        latencia[size_it][1] = (aux_lat[0] + aux_lat[1] + aux_lat[2]) / 3;
+        // Lantecy in ms
+        gettimeofday(&t2, NULL);
+        elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;
+        elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;
+        aux_lat = elapsedTime / n_it;
+
+        // Measures avarage
+        latencia[size_it][1] = aux_lat;
         vazao[size_it] = (latencia[size_it][0] * 8) / latencia[size_it][1];
         contaPacote[size_it] = conta;
         printf("\n MeanLatency: %.f miliseconds \n", latencia[size_it][1]);
         printf("\n Mean Throughput: %f bps\n", vazao[size_it]);
 
-        /*Save Results*/
+        // Save Results
         save_to_file(43, 2, latencia, vazao, contaPacote, file_name);
         free(buf);
     }
--- tp2/server.c
+++ tp2/server.c
@@ -1,57 +1,66 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<stdlib.h>
-#include<string.h>
-#include<netdb.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-#define PORT 54321
+#define PORT 33333
 #define N_BYTES 32000
 
-int main(int argc, char **argv){
-    int s; /*socket*/
-    int clen; /*Address size in bytes*/
-    int n; /*recfrom and sendto return*/
-    char buf[N_BYTES]; /*messagem buffer*/
-    int opt; /*socket flag*/
+int main(int argc, char **argv)
+{
+    int s;                     /*socket*/
+    int clen;                  /*Address size in bytes*/
+    int n;                     /*recfrom and sendto return*/
+    char buf[N_BYTES];         /*messagem buffer*/
+    int opt;                   /*socket flag*/
     struct sockaddr_in server; /*server address*/
     struct sockaddr_in client; /*client address*/
 
     /*Create socket UDP*/
     s = socket(AF_INET, SOCK_DGRAM, 0);
-    if(s<0) {
-        perror("Erro na abertura do socket");};
-    opt=1;
-    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (const void*)&opt, sizeof(int));
+    if (s < 0)
+    {
+        perror("Erro na abertura do socket");
+    };
+    opt = 1;
+    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (const void *)&opt, sizeof(int));
 
     /*Serever configuration*/
-    bzero((char*)&server, sizeof(server));
+    bzero((char *)&server, sizeof(server));
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = htonl(INADDR_ANY);
     server.sin_port = htons((unsigned short)PORT);
 
     /*socket bind*/
-    if(bind(s,(struct sockaddr *)&server, sizeof(server))<0) {
-        perror("Falha no bind.");};
+    if (bind(s, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+        perror("Falha no bind.");
+    };
 
     clen = sizeof(client);
 
     /*Send and receive message*/
-    while(1){
-        bzero(buf,N_BYTES);
+    while (1)
+    {
+        bzero(buf, N_BYTES);
 
         /*Receive*/
-        n=recvfrom(s,buf,N_BYTES,0,(struct sockaddr*)&client,&clen);
-        if(n == -1){
-            perror("ERROR in recvfrom");};
+        n = recvfrom(s, buf, N_BYTES, 0, (struct sockaddr *)&client, &clen);
+        if (n == -1)
+        {
+            perror("ERROR in recvfrom");
+        };
 
         /*Send*/
-        n=sendto(s,buf,strlen(buf),0,(struct sockaddr*)&client,clen);
-        if(n == -1){
-            perror("ERROR in sendto");};
+        n = sendto(s, buf, strlen(buf), 0, (struct sockaddr *)&client, clen);
+        if (n == -1)
+        {
+            perror("ERROR in sendto");
+        };
     }
 }
-
