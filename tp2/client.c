b1abb2f294941735c05d2b3c7a94e186a4e61bd6
Victor Moraes <victor.moraes@enacom.com.br>
2020-10-19 01:01:12 -0300

    refactor done

--- tp2/client.c
+++ tp2/client.c
@@ -12,37 +12,17 @@
 #include <sys/socket.h>
 
 #define SERVER_PORT 33333
-#define MAX_LINE 256
-#define N_BYTES 32000
-
-// Save results in file
-int save_to_file(unsigned n, unsigned m, float mat[n][m], float mat2[n], float mat3[n], char *file_name)
-{
-    FILE *pFile;
-
-    pFile = fopen(file_name, "w");
-
-    puts(file_name);
-    if (pFile != NULL)
-    {
-        fprintf(pFile, "tamanho(bytes),latencia(s),vazao(bps), Lost packet");
-        int i;
-        for (i = 0; i < n; i++)
-        {
-            fprintf(pFile, "\n %f, %f, %f, %f", mat[i][0], mat[i][1], mat2[i], mat3[i]);
-        }
-        fclose(pFile);
-    }
-}
+#define SIZE_ARR 64
+#define TOTAL_ITER 100000
 
 // Create Message
-char *generate_message(int size)
+char* generate_message(int size)
 {
     char *message;
     message = malloc(sizeof(char) * size);
     for (int i = 0; i < size - 1; i++)
     {
-        message[i] = chat((' ' + ((90 << i) | 'a' >> i ^ i << 2) % 90)) // pseudorandom character
+        message[i] = (' ' + ((90 << i) | 'a' >> i ^ i << 2) % 90); // pseudorandom character
     }
     message[size - 1] = '\0';
     return message;
@@ -51,7 +31,7 @@ char *generate_message(int size)
 int main(int argc, char *argv[])
 {
     char *host;
-    int s;
+    int udp_socket;
     int len;
     int success_ip;
     int server_len, count;
@@ -60,24 +40,16 @@ int main(int argc, char *argv[])
     struct sockaddr_in sin;
     int status;
     fd_set read_sd_set;
-    float latencia[size_array][2] = {{1, 0}, {100, 0}, {200, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {1000, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {100, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {100, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {100, 0}, {300, 0}, {400, 0}, {500, 0}, {600, 0}, {700, 0}, {800, 0}, {900, 0}, {100, 0}};
-    int latency_packet_size[32];
-    float latency_time[32];
-    int throughput_packet_size[32];
-    float throughput_time[32];
-    float vazao[size_array];
-    float contaPacote[size_array];
-    int it;
+    
     // Variables
-    float aux_lat; //Latency
-    int size, size_it;
-    int i, j;
-    int n_it = 100; //Number of Iterations
-    int conta = 0;
+    int packet_size[SIZE_ARR];
+    float latency_time[SIZE_ARR];
+    float throughput[SIZE_ARR];
+    float lost_packets_count[SIZE_ARR];
+    int lost_packets = 0;
     float elapsedTime;
 
-    // Get last test
-
+    //get current file name
     char *file_name = calloc(10, 1);
     int experiment_number = 0;
     sprintf(file_name, "result_%d.csv", experiment_number);
@@ -90,15 +62,6 @@ int main(int argc, char *argv[])
         puts(file_name);
     }
 
-    puts(file_name);
-
-    // Start latency
-    size_t size_array = sizeof(a) / sizeof(int);
-    for (it = 0; it < size_array; it++)
-    {
-        latency_packet_size[it] = i * 50
-    }
-
     // Get IP address
     host = argv[1];
     if (argc == 2)
@@ -107,15 +70,15 @@ int main(int argc, char *argv[])
     }
     else
     {
-        fprintf(stderr, "Uso: simplex-talk host \n");
+        fprintf(stderr, "Wrong input!\n Input as in: ./client host \n");
         exit(1);
     }
 
     // Create socket
-    s = socket(AF_INET, SOCK_DGRAM, 0);
-    if (s < 0)
+    udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udp_socket < 0)
     {
-        perror("simplex-talk: socket");
+        perror("Couldn't create socket");
         exit(1);
     }
 
@@ -123,7 +86,7 @@ int main(int argc, char *argv[])
     server = gethostbyname(host);
     if (server == NULL)
     {
-        fprintf(stderr, "simplex-talk: unknown host: %s \n", host);
+        fprintf(stderr, "Unknown host: %s \n", host);
         exit(1);
     }
     bzero((char *)&sin, sizeof(sin));
@@ -132,81 +95,92 @@ int main(int argc, char *argv[])
     sin.sin_port = htons(SERVER_PORT);
     server_len = sizeof(sin);
 
-    // Test for each package size
-    for (size_it = 0; size_it < size_array; size_it++)
+    // Get packet size
+    size_t size_array = sizeof(packet_size) / sizeof(int);
+    for (int i = 0; i < 31; i++) // 0 32 64 96 ... 992
     {
+        packet_size[i] = i * 32;
+        //printf("%d\n",packet_size[i]);
+    }
+    for (int i = 31; i < 64; i++) // 1024 2048 3072 ... 32700
+    {
+        packet_size[i] = (i - 31) * 1024;
+        //printf("%d, %d\n",packet_size[i], i);
+    }
+    int size = 0;
+    // Test for each package size
 
+    for (int i_pkt = 0; i_pkt < size_array; i_pkt++)
+    {
+        //printf("%d, %d\n", packet_size[i_pkt], i_pkt);
         // Create message
-        size = latency_packet_size[size_it - 1];
+        size = packet_size[i_pkt];
         char *buf = generate_message(size);
-        printf("\n\n Packet Size: %d Bytes", size);
+        //printf("\n\n Packet Size: %d Bytes", size);
 
         // Start lost packet count
-        conta = 0;
+        lost_packets = 0;
 
-        // Get time
+        // Get start time
         gettimeofday(&t1, NULL);
 
         // Start Test
-        for (i = 0; i < n_it; i++)
+        for (int i = 0; i < TOTAL_ITER; i++)
         {
-
             // Send Message
-            count = sendto(s, buf, strlen(buf), 0, (struct sockaddr *)&sin, server_len);
+            count = sendto(udp_socket, buf, strlen(buf), 0, (struct sockaddr *)&sin, server_len);
             if (count < 0)
                 perror("Transmiting error.");
             FD_ZERO(&read_sd_set);
-            FD_SET(s, &read_sd_set);
+            FD_SET(udp_socket, &read_sd_set);
             tv.tv_sec = 1;
             tv.tv_usec = 0;
-            status = select(s + 1, &read_sd_set, NULL, NULL, &tv);
-            if (FD_ISSET(s, &read_sd_set))
+            status = select(udp_socket + 1, &read_sd_set, NULL, NULL, &tv);
+            if (FD_ISSET(udp_socket, &read_sd_set))
             {
-
-                // Get Message
-                count = recvfrom(s, buf, strlen(buf), 0, (struct sockaddr *)&sin, &server_len);
+                // Receive Message
+                count = recvfrom(udp_socket, buf, strlen(buf), 0, (struct sockaddr *)&sin, &server_len);
                 if (count < 0)
                     perror("Receiving error.");
             }
             else
             {
                 printf("TIMEOUT UDP");
-                conta = conta + 1;
+                lost_packets = lost_packets + 1;
             }
+            //printf("%d, %d\n", packet_size[i_pkt], i_pkt);
         }
 
         // Lantecy in ms
         gettimeofday(&t2, NULL);
         elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;
         elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;
-        aux_lat = elapsedTime / n_it;
 
         // Measures avarage
-        latency_time[i] = aux_lat;
-        vazao[i ] = (latencia[size_it][0] * 8) / latency_time[size_it];
-        contaPacote[size_it] = conta;
-        printf("\n MeanLatency: %.f miliseconds \n", latency_time[size_it]);
-        printf("\n Mean Throughput: %f bps\n", vazao[size_it]);
-
-        // Save Results
-        save_to_file(size_array, 2, latency_time, vazao, contaPacote, file_name);
-
-        FILE *pFile;
-
-        pFile = fopen(file_name, "w");
-
-        puts(file_name);
-        if (pFile != NULL)
-        {
-            fprintf(pFile, "tamanho(bytes),latencia(s),vazao(bps), Lost packet");
-            int i;
-            for (i = 0; i < n; i++)
-            {
-                fprintf(pFile, "\n %f, %f, %f, %f", mat[i][0], mat[i][1], mat2[i], mat3[i]);
-            }
-            fclose(pFile);
-        }
+        latency_time[i_pkt] = elapsedTime / TOTAL_ITER;
+        throughput[i_pkt] = (packet_size[i_pkt] * 8) / latency_time[i_pkt];
+        lost_packets_count[i_pkt] = lost_packets;
+        printf("\n Packet size %d \n", packet_size[i_pkt]);
+        printf("\n MeanLatency: %.f miliseconds \n", latency_time[i_pkt]);
+        printf("\n Mean Throughput: %f bps\n", throughput[i_pkt]);
         free(buf);
     }
-    close(s);
+
+    // Save Results to csv
+
+    FILE *pFile;
+
+    pFile = fopen(file_name, "w");
+
+    if (pFile != NULL)
+    {
+        fprintf(pFile, "packetSize(bytes),latency(s),throughput(bps), LostPacket");
+        for (int i = 0; i < size_array; i++)
+        {
+            fprintf(pFile, "\n %d, %f, %f, %f", packet_size[i], latency_time[i],
+                    throughput[i], lost_packets_count[i]);
+        }
+        fclose(pFile);
+    }
+    close(udp_socket);
 }
